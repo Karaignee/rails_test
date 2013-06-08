023960cf4f4745478a6b940c40e333050ba0b770
Karaignee <karen_rooney@hotmail.com>
2013-06-08 21:56:40 +0100

    added some very basic TwBS styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419174931) do
+ActiveRecord::Schema.define(:version => 20130607104227) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130419174931) do
     t.integer  "price"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image"
   end
 
 end
